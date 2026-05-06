92ae5df17a9706172d491439c8d02ffe97d4befd
komilazio <victordumbiri88@gmail.com>
2026-05-06 05:40:06 +0300

    mode indicators

--- init.lua
+++ init.lua
@@ -4,16 +4,17 @@ vim.o.winborder = "rounded"
 vim.opt.laststatus = 3
 vim.opt.tabstop = 6
 vim.opt.shiftwidth = 6
+vim.opt.showmode = false
 vim.opt.scrolloff = 9
 vim.opt.termguicolors = true
 vim.opt.expandtab = true
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- vim.opt.number = true
+-- vim.opt.relativenumber = true
 -- vim.opt.virtualedit = "all"
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 vim.opt.smartindent = true
 vim.g.rust_recommended_style = false
-vim.opt.signcolumn = "no"
+-- vim.opt.signcolumn = "yes:1"
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.backup = false
@@ -69,9 +70,13 @@ vim.pack.add({
       "https://github.com/dhruvasagar/vim-table-mode",
       "https://github.com/ej-shafran/compile-mode.nvim",
       "https://github.com/mbbill/undotree",
+      "https://github.com/jpwol/thorn.nvim",
 })
+-- after plugin is loaded by your manager
+-- vim.cmd([[colorscheme thorn]])
+
 require("mini.icons").setup()
-require('lualine').setup()
+-- require('lualine').setup()
 require("which-key").setup({ })
 require("mason").setup({ })
 require("winshift").setup()
@@ -397,13 +402,257 @@ vim.api.nvim_create_autocmd("TermOpen", {
       end,
 })
 
+-- Eviline config for lualine
+-- Author: shadmansaleh
+-- Credit: glepnir
+local lualine = require('lualine')
+-- Color table for highlights
+-- stylua: ignore
+local colors = {
+      bg       = '#141414',
+      fg       = '#bbc2cf',
+      yellow   = '#ECBE7B',
+      cyan     = '#008080',
+      darkblue = '#081633',
+      green    = '#98be65',
+      orange   = '#FF8800',
+      violet   = '#a9a1e1',
+      magenta  = '#c678dd',
+      blue     = '#51afef',
+      red      = '#ec5f67',
+}
+local conditions = {
+      buffer_not_empty = function()
+            return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+      end,
+      hide_in_width = function()
+            return vim.fn.winwidth(0) > 80
+      end,
+      check_git_workspace = function()
+            local filepath = vim.fn.expand('%:p:h')
+            local gitdir = vim.fn.finddir('.git', filepath .. ';')
+            return gitdir and #gitdir > 0 and #gitdir < #filepath
+      end,
+}
+-- Config
+local config = {
+      options = {
+            -- Disable sections and component separators
+            component_separators = '',
+            section_separators = '',
+            theme = {
+                  -- We are going to use lualine_c an lualine_x as left and
+                  -- right section. Both are highlighted by c theme .  So we
+                  -- are just setting default looks o statusline
+                  normal = { c = { fg = colors.fg, bg = "#181818"  } }, --colors.bg
+                  inactive = { c = { fg = colors.fg, bg = colors.bg } },
+            },
+      },
+      sections = {
+            -- these are to remove the defaults
+            lualine_a = {},
+            lualine_b = {},
+            lualine_y = {},
+            lualine_z = {},
+            -- These will be filled later
+            lualine_c = {},
+            lualine_x = {},
+      },
+      inactive_sections = {
+            -- these are to remove the defaults
+            lualine_a = {},
+            lualine_b = {},
+            lualine_y = {},
+            lualine_z = {},
+            lualine_c = {},
+            lualine_x = {},
+      },
+}
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+      table.insert(config.sections.lualine_c, component)
+end
+-- Inserts a component in lualine_x at right section
+local function ins_right(component)
+      table.insert(config.sections.lualine_x, component)
+end
+
+local the_bar = function ()
+
+ins_left {
+      function()
+            -- Mode indicator
+            return "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰"
+      end,
+      -- color = { fg = "#008080" },
+      color = function()
+            -- auto change color according to neovims mode
+            local mode_color = {
+                  n = colors.orange,
+                  i = "#09E717",
+                  v = "#510974",
+                  [''] = colors.blue,
+                  V = "#510974",
+                  c = "#FF0000",
+                  no = colors.red,
+                  s = colors.orange,
+                  S = colors.orange,
+                  [''] = colors.orange,
+                  ic = colors.yellow,
+                  R = colors.violet,
+                  Rv = colors.violet,
+                  cv = colors.red,
+                  ce = colors.red,
+                  r = colors.cyan,
+                  rm = colors.cyan,
+                  ['r?'] = colors.cyan,
+                  ['!'] = colors.red,
+                  t = colors.red,
+            }
+            return { fg = mode_color[vim.fn.mode()] }
+      end,
+      padding = { left = 2, right = 0 }, -- We don't need space before this
+}
+end
+
+ins_left {
+      function()
+           return "⟦⟪⟫⟧"
+            -- return "▞▚"
+      end,
+      color = { fg = "#9D62D3" },
+      padding = { left = 0, right = 1 }, -- We don't need space before this
+}
+ins_left {
+      -- filesize component
+      'filesize',
+      cond = conditions.buffer_not_empty,
+}
+ins_left {
+      'filename',
+      cond = conditions.buffer_not_empty,
+      color = { fg = colors.yellow },
+}
+
+ins_left { 'location' }
+
+ins_left { 'progress', color = { fg = colors.fg } }
+
+ins_left {
+      'diagnostics',
+      sources = { 'nvim_diagnostic' },
+      symbols = { error = ' ', warn = ' ', info = ' ' },
+      diagnostics_color = {
+            error = { fg = colors.red },
+            warn = { fg = colors.yellow },
+            info = { fg = colors.cyan },
+      },
+}
+
+the_bar()
+
+-- Insert mid section. You can make any number of sections in neovim :)
+-- for lualine it's any number greater then 2
+ins_left {
+      function()
+            return '%='
+      end,
+}
+
+
+ins_left {
+      -- Lsp server name .
+      function()
+            local msg = 'No Active Lsp'
+            local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
+            local clients = vim.lsp.get_clients()
+            if next(clients) == nil then
+                  return msg
+            end
+            for _, client in ipairs(clients) do
+                  local filetypes = client.config.filetypes
+                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                        return client.name
+                  end
+            end
+            return msg
+      end,
+      icon = '',
+      color = { fg = '#CDE3C8' },
+}
+
+ins_right {
+      function()
+            return '%='
+      end,
+}
+
+
+-- Add components to right sections
+ins_right {
+      'o:encoding', -- option component same as &encoding in viml
+      fmt = string.upper, -- I'm not sure why it's upper case either ;)
+      cond = conditions.hide_in_width,
+      color = { fg = colors.green },
+}
+
+
+
+ins_right {
+      'fileformat',
+      fmt = string.upper,
+      icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
+      color = { fg = colors.green },
+}
+
+ins_right {
+      'branch',
+      icon = '',
+      color = { fg = colors.violet },
+}
+
+ins_right {
+      'diff',
+      -- Is it me or the symbol for modified us really weird
+      symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
+      diff_color = {
+            added = { fg = colors.green },
+            modified = { fg = colors.orange },
+            removed = { fg = colors.red },
+      },
+      cond = conditions.hide_in_width,
+}
+
+ins_right {
+      function()
+           return "⟦⟪⟫⟧"
+            -- return "▞▚"
+      end,
+      color = { fg = "#9D62D3" },
+      padding = { left = 0, right = 0 },
+}
+-- Now don't forget to initialize lualine
+lualine.setup(config)
+
 vim.lsp.enable({
       "rust_analyzer",
       "ols",
       "luals",
       "clangd",
 })
-vim.diagnostic.config({ virtual_text = true })
+
+vim.diagnostic.config({
+      signs = {
+            text = {
+                  ERROR = " ",
+                  WARN  = " ",
+                  HINT  = " ",
+                  INFO  = " ",
+            },
+      },
+      virtual_text = true,
+})
+vim.o.signcolumn = "yes"
 vim.keymap.set("n", "<leader>m", function()
       vim.diagnostic.open_float(nil, { focus = false })
 end, {desc = "View Diagnostics Error",
